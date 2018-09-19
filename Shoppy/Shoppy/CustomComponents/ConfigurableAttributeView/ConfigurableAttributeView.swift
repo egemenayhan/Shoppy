cd7f39294470857a939e2d1f8988c7994698c445
Egemen Ayhan <egemen.ayhan@turkcell.com.tr>
2018-09-19 18:43:38 +0300

    Shoppy: ConfigurableAttributeView added

---
+++ Shoppy/Shoppy/CustomComponents/ConfigurableAttributeView/ConfigurableAttributeView.swift
@@ -0,0 +1,71 @@
+//
+//  ConfigurableAttributeView.swift
+//  Shoppy
+//
+//  Created by EGEMEN AYHAN on 19.09.2018.
+//  Copyright © 2018 Egemen Ayhan. All rights reserved.
+//
+
+import UIKit
+import Networking
+
+// MARK: - ConfigurableAttributeDelegate
+protocol ConfigurableAttributeDelegate {
+    
+    func didTap(attributeView: ConfigurableAttributeView)
+
+}
+
+// MARK: - ConfigurableAttributeView
+class ConfigurableAttributeView: UIView {
+
+    // MARK: - Outlets
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    // MARK: - Properties
+    private(set) var configurableAttribute: ConfigurableAttribute! {
+        didSet {
+            setupUI()
+        }
+    }
+    var selectedOption: ConfigurableAttributeOption? {
+        didSet {
+            updateUI()
+        }
+    }
+    var delegate: ConfigurableAttributeDelegate?
+    
+    // MARK: - Lifecycle
+    static func instantiate(for attribute: ConfigurableAttribute) -> ConfigurableAttributeView {
+        guard let view = UINib(nibName: String(describing: ConfigurableAttributeView.self), bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? ConfigurableAttributeView else {
+            fatalError("Can not find nib for class: " + String(describing: ConfigurableAttributeView.self))
+        }
+        view.configurableAttribute = attribute
+        
+        return view
+    }
+
+}
+
+// MARK: - Private extension
+private extension ConfigurableAttributeView {
+    
+    func setupUI() {
+        let gestureRecognizer = UIGestureRecognizer(target: self, action: #selector(attributeTapped))
+        self.addGestureRecognizer(gestureRecognizer)
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        let type = configurableAttribute.type.rawValue
+        typeLabel.text = type
+        valueLabel.text = selectedOption?.title ?? "Select \(type)"
+    }
+    
+    @objc func attributeTapped() {
+        delegate?.didTap(attributeView: self)
+    }
+    
+}
